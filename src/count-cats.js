d396ec8462cf0565a6fff84669a172c258363177
Maxim Liplyanin <liplyanin@yandex.ru>
2020-02-18 19:36:34 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,6 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arrayOfArr) {
+  let result = arrayOfArr.reduce((resultCount, arr) =>
+  resultCount + arr.reduce((countArr, row) => 
+  countArr + (row == '^^' ? 1 : 0),0),0);
+  return result;
 };
