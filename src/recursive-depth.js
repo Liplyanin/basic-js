787204fdfc7e01a120325ade8d7cad1540c134e8
Maxim Liplyanin <liplyanin@yandex.ru>
2020-02-24 21:45:02 +0300

    add solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,19 +1,10 @@
 module.exports = class DepthCalculator {
-    calculateDepth( arr) { 
-        let depth = 1;
-        if(Array.isArray(arr)&&arr.length===0){
-   
-                return depth;
-            }else if(Array.isArray(arr)){
-  
-                arr.forEach(element => {
-                    if(Array.isArray(element)){
-                        
-                        return depth +=this.calculateDepth(element)
-                    }
-                });
-  
-            } else return 0;       
-     return depth;
+    calculateDepth(arr ) {
+        if(arr.every(el=>!Array.isArray(el))){
+            return 1;
+        }else{
+            return 1  + this.calculateDepth(arr.flat());
+        }
     }
-  };
+};
+
