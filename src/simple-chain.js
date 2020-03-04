270d978a2bf56172f22ce4ec4d5d24ede969be3f
Maxim Liplyanin <liplyanin@yandex.ru>
2020-03-04 14:14:04 +0300

    add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,37 @@
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.push(String(value));
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position > 0 && position <= this.arr.length){
+        this.arr.splice(position-1, 1);
+        return this;
+      }else {
+      this.arr = [];
+      throw new Error;
+    }
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+   
+    let result = this.arr;
+    this.arr = [];
+    return '( ' + result.join(' )~~( ') + ' )';
   }
+
 };
 
 module.exports = chainMaker;
